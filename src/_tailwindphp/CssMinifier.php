50875702cb82045ef1f384bb964cf912fe78edaf
dnnsjsk <dnnsjsk@gmail.com>
2026-04-24 20:25:41 +0200

    Fix vendor-safe inference and math minification

--- src/_tailwindphp/CssMinifier.php
+++ src/_tailwindphp/CssMinifier.php
@@ -79,8 +79,13 @@ class CssMinifier
                 $prev = $out !== '' ? substr($out, -1) : '';
                 $next = $i + 1 < $len ? $css[$i + 1] : '';
 
-                $stripAfter = '{};:,(';
+                $stripAfter = '{};:(';
                 $stripBefore = '{};:,)';
+
+                if ($depth === 0) {
+                    $stripAfter .= ',';
+                }
+
                 if (str_contains($stripAfter, $prev) || str_contains($stripBefore, $next)) {
                     continue;
                 }
